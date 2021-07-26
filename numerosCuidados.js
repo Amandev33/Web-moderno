739c1813493b7ad38a5f2ba5c4f3b09e6e0b66c4
Amdev <86101487+Amandev33@users.noreply.github.com>
2021-07-26 18:14:50 -0300

    Update numerosCuidados.js

--- numerosCuidados.js
+++ numerosCuidados.js
@@ -1,6 +1,6 @@
 console.log(7 / 0.001)
 console.log("10" / 2) //dentro da string, pode ser convertido em número
 console.log(0.1 + 0.7) // gera imprecisão pq senão seria muito demorado
-//console.log("Show!" * 2) Not a Numbwe NaN
-//console.log(10.toString())
+//console.log("Show!" * 2) Not a Number NaN
+//console.log(10.toString()) NaN
 console.log((10.345).toFixed(2))
